50cb00081ba8f786a75c4e35e8b88f808db40bd2
Dandelikeswood <j.boudouresque@gmail.com>
2023-02-05 12:50:06 +0100

    Menu update

--- Assets/DEV_JEANNE/Scripts/Main_menu.cs
+++ Assets/DEV_JEANNE/Scripts/Main_menu.cs
@@ -1,18 +1,75 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Main_menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string levelToLoad;
+
+    public GameObject HowtoPlayWindow1;
+    public GameObject creditsWindow;
+    public GameObject settingsWindow;
+    public GameObject HowtoPlayWindow2;
+    [SerializeField] private AudioSource buttonclick;
+
+    public void StartGame()
     {
-        
+        buttonclick.Play();
+        SceneManager.LoadScene(levelToLoad);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenHowtoPlayWindow()
     {
-        
+        buttonclick.Play();
+        HowtoPlayWindow1.SetActive(true);
+    }
+
+    public void OpenCreditsWindow()
+    {
+        buttonclick.Play();
+        creditsWindow.SetActive(true);
+    }
+    public void OpenSettingsWindow()
+    {
+        buttonclick.Play();
+        settingsWindow.SetActive(true);
+    }
+    public void OpenNextPageHTP()
+    {
+        buttonclick.Play();
+        HowtoPlayWindow2.SetActive(true);
+        HowtoPlayWindow1.SetActive(false);
+    }
+    public void CloseNextPageHTP()
+    {
+        buttonclick.Play();
+        HowtoPlayWindow2.SetActive(false);
+    }
+    public void ReturnPastPageHTP()
+    {
+        buttonclick.Play();
+        HowtoPlayWindow2.SetActive(false);
+        HowtoPlayWindow1.SetActive(true);
+    }
+    public void CloseHowtoPlayWindow()
+    {
+        buttonclick.Play();
+        HowtoPlayWindow1.SetActive(false);
+    }
+
+    public void CloseCreditsWindow()
+    {
+        buttonclick.Play();
+        creditsWindow.SetActive(false);
+    }
+    public void CloseSettingsWindow()
+    {
+        buttonclick.Play();
+        settingsWindow.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        buttonclick.Play();
+        Application.Quit();
     }
 }
